d653b341c846421f492083c5b3a6c7b0fead5e1d
jonatan-elizalde-gomez <jonatanelizaldegomez@gmail.com>
2024-03-12 14:04:59 -0600

    Feature: service added and pokemon list component created

---
+++ src/app/models/pokemon/pokemon.model.ts
@@ -0,0 +1,41 @@
+export interface PokemonList {
+    count: number;
+    next: string;
+    previous: string;
+    results: Array<PokemonListItem>;
+  }
+  
+  export interface PokemonListItem {
+    name: string;
+    url: string;
+  }
+  
+  export interface PokemonDetails {
+    abilities: Ability[];
+    height: number;
+    weight: number;
+    name: string;
+    types: Type[];
+  }
+  
+  export interface Ability {
+    ability: AbilityDetail;
+    is_hidden: boolean;
+    slot: number;
+  }
+  
+  export interface AbilityDetail {
+    name: string;
+    url: string;
+  }
+  
+  export interface Type {
+    slot: number;
+    type: TypeDetail;
+  }
+  
+  export interface TypeDetail {
+    name: string;
+    url: string;
+  }
+  
---
+++ src/app/services/pokemon.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { PokemonList, PokemonDetails } from '../models/pokemon/pokemon.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PokemonService {
+
+  private baseUrl: string = 'https://pokeapi.co/api/v2';
+
+  constructor(private http: HttpClient) { }
+
+  getPokemonList(limit: number, offset: number): Observable<PokemonList> {
+    return this.http.get<PokemonList>(`${this.baseUrl}/pokemon?limit=${limit}&offset=${offset}`);
+  }
+
+  getPokemonDetails(id: number): Observable<PokemonDetails> {
+    return this.http.get<PokemonDetails>(`${this.baseUrl}/pokemon/${id}`);
+  }
+}
